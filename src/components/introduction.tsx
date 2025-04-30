52b2019ad0675f5b3e0ff89ae77403d9ed89e548
西川雅人 <masato.nishikawa@arteryex.biz>
2025-04-30 16:17:08 +0900

    強制できるのか

--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Introduction = () => {
   return (
-    <div className="text-center text-white bg-red-500">自己紹介の部分です</div>
+    <div className="text-center text-white bg-red-500">
+      自己紹介の部分です。
+    </div>
   );
 };
 
